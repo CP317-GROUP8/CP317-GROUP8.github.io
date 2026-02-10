e3e137b9f8a515d53b3c47d8771ede373fbde9fd
Janya Walia <163579753+Janya282@users.noreply.github.com>
2026-02-09 20:59:16 -0500

    working on making homepage look functional

--- home.js
+++ home.js
@@ -1,23 +1,14 @@
 function requireLogin() {
   const userStr = localStorage.getItem("user");
   if (!userStr) window.location.href = "index.html";
-  return JSON.parse(userStr);
 }
+requireLogin();
 
-const user = requireLogin();
-document.getElementById("hello").textContent = `Hi, ${user.firstName || "User"}`;
+document.getElementById("searchBtn").addEventListener("click", () => {
+  const where = document.getElementById("whereInput").value.trim();
+  const pickup = document.getElementById("pickupInput").value;
+  const dropoff = document.getElementById("dropoffInput").value;
 
-document.getElementById("logout").addEventListener("click", () => {
-  localStorage.removeItem("user");
-  window.location.href = "index.html";
-});
-
-document.getElementById("goSearch").addEventListener("click", () => {
-  const where = document.getElementById("where").value.trim();
-  const pickup = document.getElementById("pickup").value;
-  const dropoff = document.getElementById("dropoff").value;
-
-  // pass search inputs to cars page (even if backend doesn't filter yet)
   const params = new URLSearchParams();
   if (where) params.set("where", where);
   if (pickup) params.set("pickup", pickup);
@@ -25,3 +16,17 @@ document.getElementById("goSearch").addEventListener("click", () => {
 
   window.location.href = `cars.html?${params.toString()}`;
 });
+
+document.getElementById("iconHome").addEventListener("click", () => {
+  window.location.href = "home.html";
+});
+
+document.getElementById("iconCars").addEventListener("click", () => {
+  window.location.href = "cars.html";
+});
+
+document.getElementById("iconLogout").addEventListener("click", () => {
+  localStorage.removeItem("user");
+  window.location.href = "index.html";
+});
+
